9e2335979cfc6cb4e4b563d887dd18cd61755b8e
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-16 08:53:24 +0530

    Final Commit of Star Rating

---
+++ StarRating/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StarRating from "./StarRating";
+
+const App = () => {
+  return (
+    <div>
+      <StarRating noOfStars={10} />
+    </div>
+  );
+};
+
+export default App;
--- StarRating/vite-project/src/StarRating.jsx
+++ StarRating/vite-project/src/StarRating.jsx
@@ -1,30 +1,32 @@
 import { React, useState } from "react";
 import { FaStar } from "react-icons/fa";
+import "./index.css";
 
 export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   function handleClick(getCurrentIndex) {
-    console.log(getCurrentIndex);
+    setRating(getCurrentIndex);
   }
 
   function handleMouseEnter(getCurrentIndex) {
-    console.log(getCurrentIndex);
+    setHover(getCurrentIndex);
   }
 
-  function handleMouse(getCurrentIndex) {
-    console.log(getCurrentIndex);
+  function handleMouse() {
+    setHover(rating);
   }
 
   return (
-    <div>
+    <div className="star-rating">
       <h1>Star Rating Component</h1>
       {[...Array(noOfStars)].map((_, index) => {
         index += 1;
         return (
           <FaStar
             key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
             onClick={() => {
               handleClick(index);
             }}
